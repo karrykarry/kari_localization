66a0b34195b2891b90828202b33f520ec40b9c60
kari <12kari20@gmail.com>
2018-06-15 21:15:17 +0900

    registration

--- kari_localization/src/registration_master.cpp
+++ kari_localization/src/registration_master.cpp
@@ -23,68 +23,109 @@
 #include <local_tool/mathematics.hpp>
 #include <local_tool/registration.hpp>
 
-// #include "registration.hpp"
-// #include "mathematics.hpp"
-// #include "filters_kari.hpp"
-
 
 using namespace std;
 
 
-int main (int argc, char** argv)
+class Regi
 {
+	private:
+		ros::NodeHandle n;
+		ros::Rate r;
 
-	double l_roll, l_pitch, l_yaw;//角度etc
+		ros::Publisher trg_pub;
+		ros::Publisher ipt_pub;
+		ros::Publisher out_pub;
+		ros::Publisher map_pub;
 
-	ros::init(argc, argv, "registration_master");
-	ros::NodeHandle n;
-	ros::Rate roop(1);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud;
 
+		string file_input;
+		string file_target;
 
-	ros::Publisher trg_pub  = n.advertise<sensor_msgs::PointCloud2>("/target_cloud", 1);
-	ros::Publisher ipt_pub = n.advertise<sensor_msgs::PointCloud2>("/input_cloud", 1);
-	ros::Publisher out_pub = n.advertise<sensor_msgs::PointCloud2>("/output_cloud", 1);
+		string file;
 
-	// Loading first scan of room.
-	pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	public:
+		Regi(ros::NodeHandle& n);
 
-//////////filtersの確認//////////////
-	ros::Publisher map_pub  = n.advertise<sensor_msgs::PointCloud2>("/map_cloud", 1);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-	string file;
-	sensor_msgs::PointCloud2 pc4;
-	
-	file = "/home/amsl/onda_map/d_kan_around_si2017_gicp_ds.pcd";
-	map_reader(0.5,file,map_cloud);
-	pcl::toROSMsg(*map_cloud, pc4);
-/////////////////////////////////////
+		void alignment(void);
+		void cloud_data(void);
+		void map(void);
 
-	string file_input;
-	string file_target;
+		bool spin()
+		{
+			ros::Rate loop_rate(r);
+			cloud_data();
 
+			while(ros::ok()){
+				alignment();
+				// map();	//mapの確認
+				ros::spinOnce();
+				loop_rate.sleep();
+
+			}
+			return true;
+		}
+
+};
+
+Regi::Regi(ros::NodeHandle &n) :
+	r(1)
+{
+	trg_pub  = n.advertise<sensor_msgs::PointCloud2>("/target_cloud", 1);
+	ipt_pub  = n.advertise<sensor_msgs::PointCloud2>("/input_cloud", 1);
+	out_pub  = n.advertise<sensor_msgs::PointCloud2>("/output_cloud", 1);
+	map_pub  = n.advertise<sensor_msgs::PointCloud2>("/map_cloud", 1);
 
 	n.getParam("input/cloud",file_input);
 	n.getParam("target/cloud",file_target);
 
+	target_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
+	input_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
+	output_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
+	map_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
+
+
+	file = "/home/amsl/onda_map/d_kan_around_si2017_gicp_ds.pcd";
+	map_reader(0.5,file,map_cloud);
+
+}
+
+void
+Regi::cloud_data (void){
+
 	pcl::io::loadPCDFile(file_input, *input_cloud);
 	pcl::io::loadPCDFile(file_target, *target_cloud);
 
+}
 
 
-	tf::TransformBroadcaster br;
-	tf::Transform transform;
+
+void
+Regi::map (void){
+
+	sensor_msgs::PointCloud2 pc4;
+	
+	pcl::toROSMsg(*map_cloud,pc4);
+
+	pc4.header.frame_id = "map";
+	pc4.header.stamp  = ros::Time::now();
+	map_pub.publish(pc4);
+
+}
+
+
+void
+Regi::alignment(void){
 
 	Eigen::Matrix4f a;
+	double l_roll, l_pitch, l_yaw;//角度etc
 
 	// a = registration_ndt_vis(target_cloud,input_cloud,output_cloud);
 	a = registration_icp_vis(target_cloud,input_cloud,output_cloud);
-
-	cout<<"ndt"<<endl;
-
-	// a = ndt.getFinalTransformation ();////4×4の行列         
-	//最適の角度・長さを検出
 
 	calc_rpy(a,l_roll,l_pitch,l_yaw);
 	
@@ -102,29 +143,30 @@ int main (int argc, char** argv)
 	pcl::toROSMsg(*input_cloud, pc2);
 	pcl::toROSMsg(*output_cloud, pc3);
 
+	pc.header.frame_id  = "map";
+	pc2.header.frame_id = "map";
+	pc3.header.frame_id = "map";
 
-	while(ros::ok()){
+	pc.header.stamp  = ros::Time::now();
+	pc2.header.stamp = ros::Time::now();
+	pc3.header.stamp = ros::Time::now();
 
-		pc.header.frame_id  = "map";
-		pc2.header.frame_id = "map";
-		pc3.header.frame_id = "map";
+	trg_pub.publish(pc);
+	ipt_pub.publish(pc2);
+	out_pub.publish(pc3);
 
-		pc.header.stamp  = ros::Time::now();
-		pc2.header.stamp = ros::Time::now();
-		pc3.header.stamp = ros::Time::now();
+}
 
-		trg_pub.publish(pc);
-		ipt_pub.publish(pc2);
-		out_pub.publish(pc3);
+int main (int argc, char** argv)
+{
+	ros::init(argc, argv, "registration_master");
+	ros::NodeHandle n;
 
-/////////////filtersの確認/////////////
-		pc4.header.frame_id = "map";
-		pc4.header.stamp  = ros::Time::now();
-		map_pub.publish(pc4);
-//////////////////////////////////////
-		roop.sleep();
-		ros::spinOnce();
-	}
+	cout<<"-------registration start---------"<<endl;
+
+	Regi regi(n);
+
+	regi.spin();
 
 	return (0);
 }
