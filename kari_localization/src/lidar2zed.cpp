33d864aa501c22dbf2d72312672e04e93293cd65
kari <12kari20@gmail.com>
2018-06-16 17:41:20 +0900

    lidar2zed

--- kari_localization/src/lidar2zed.cpp
+++ kari_localization/src/lidar2zed.cpp
@@ -36,29 +36,35 @@ class Regi
 		ros::Publisher trg_pub;
 		ros::Publisher ipt_pub;
 		ros::Publisher out_pub;
-		ros::Publisher map_pub;
+
+		ros::Subscriber lidar_sub;
+		ros::Subscriber camera_sub;
+
 
 		pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud;
 		pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;
 		pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud;
-		pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud;
 
-		string file_input;
-		string file_target;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr lidar_cloud;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr camera_cloud;
 
-		string file;
+
+		string LIDAR_TOPIC;
+		string CAMERA_TOPIC;
+		
+		string PARENT_FRAME;
 
 	public:
 		Regi(ros::NodeHandle& n);
 
 		void alignment(void);
-		void cloud_data(void);
 		void map(void);
-
+		
+		void lidarCallback(const sensor_msgs::PointCloud2ConstPtr msgs);
+		void cameraCallback(const sensor_msgs::PointCloud2ConstPtr msgs);
 		bool spin()
 		{
 			ros::Rate loop_rate(r);
-			cloud_data();
 
 			while(ros::ok()){
 				alignment();
@@ -73,48 +79,43 @@ class Regi
 };
 
 Regi::Regi(ros::NodeHandle &n) :
-	r(1)
+	r(7)
 {
+
+	n.getParam("lidar_topic",LIDAR_TOPIC);
+	n.getParam("camera_topic",CAMERA_TOPIC);
+	n.getParam("parent_frame",PARENT_FRAME);
+	
 	trg_pub  = n.advertise<sensor_msgs::PointCloud2>("/target_cloud", 1);
 	ipt_pub  = n.advertise<sensor_msgs::PointCloud2>("/input_cloud", 1);
-	out_pub  = n.advertise<sensor_msgs::PointCloud2>("/output_cloud", 1);
-	map_pub  = n.advertise<sensor_msgs::PointCloud2>("/map_cloud", 1);
+	out_pub  = n.advertise<sensor_msgs::PointCloud2>("/lidar2zed", 1);
 
-	n.getParam("input/cloud",file_input);
-	n.getParam("target/cloud",file_target);
+
+	lidar_sub = n.subscribe(LIDAR_TOPIC,1000,&Regi::lidarCallback,this);
+	camera_sub = n.subscribe(CAMERA_TOPIC,1000,&Regi::cameraCallback,this);
+
 
 	target_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
 	input_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
 	output_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
-	map_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
 
-
-	file = "/home/amsl/onda_map/d_kan_around_si2017_gicp_ds.pcd";
-	map_reader(0.5,file,map_cloud);
-
-}
-
-void
-Regi::cloud_data (void){
-
-	pcl::io::loadPCDFile(file_input, *input_cloud);
-	pcl::io::loadPCDFile(file_target, *target_cloud);
+	lidar_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
+	camera_cloud.reset (new pcl::PointCloud<pcl::PointXYZ>);
 
 }
 
 
+void
+Regi::lidarCallback(const sensor_msgs::PointCloud2ConstPtr msgs){
+
+	pcl::fromROSMsg(*msgs,*lidar_cloud);
+
+}
 
 void
-Regi::map (void){
+Regi::cameraCallback(const sensor_msgs::PointCloud2ConstPtr msgs){
 
-	sensor_msgs::PointCloud2 pc4;
-	
-	pcl::toROSMsg(*map_cloud,pc4);
-
-	pc4.header.frame_id = "map";
-	pc4.header.stamp  = ros::Time::now();
-	map_pub.publish(pc4);
-
+	pcl::fromROSMsg(*msgs,*camera_cloud);
 }
 
 
@@ -124,9 +125,11 @@ Regi::alignment(void){
 	Eigen::Matrix4f a;
 	double l_roll, l_pitch, l_yaw;//角度etc
 
+
 	//icpはinputをtargetに持っていく input = output
+	// a = registration_icp_vis(target_cloud,input_cloud,output_cloud);
 	// a = registration_ndt_vis(target_cloud,input_cloud,output_cloud);
-	a = registration_icp_vis(target_cloud,input_cloud,output_cloud);
+	a = registration_icp_vis(lidar_cloud,camera_cloud,output_cloud);
 
 	calc_rpy(a,l_roll,l_pitch,l_yaw);
 	
@@ -134,19 +137,19 @@ Regi::alignment(void){
 	//
 	cout<<"t(0,3) : "<<a(0, 3)<<endl;
 	cout<<"t(1,3) : "<<a(1, 3)<<endl;
-	cout<<"t(2,3) : "<<a(1, 3)<<endl;
+	cout<<"t(2,3) : "<<a(2, 3)<<endl;
 	cout<<"l_roll : "<<l_roll<<endl;
 	cout<<"l_pitch : "<<l_pitch<<endl;
 	cout<<"l_yaw : "<<l_yaw<<endl;
 
 	sensor_msgs::PointCloud2 pc, pc2, pc3;
-	pcl::toROSMsg(*target_cloud, pc);
-	pcl::toROSMsg(*input_cloud, pc2);
+	pcl::toROSMsg(*camera_cloud, pc);
+	pcl::toROSMsg(*lidar_cloud, pc2);
 	pcl::toROSMsg(*output_cloud, pc3);
 
-	pc.header.frame_id  = "map";
-	pc2.header.frame_id = "map";
-	pc3.header.frame_id = "map";
+	pc.header.frame_id  = "odom";
+	pc2.header.frame_id = "odom";
+	pc3.header.frame_id = "centerlaser";
 
 	pc.header.stamp  = ros::Time::now();
 	pc2.header.stamp = ros::Time::now();
@@ -160,10 +163,10 @@ Regi::alignment(void){
 
 int main (int argc, char** argv)
 {
-	ros::init(argc, argv, "registration_master");
+	ros::init(argc, argv, "lidar2zed");
 	ros::NodeHandle n;
 
-	cout<<"-------registration start---------"<<endl;
+	cout<<"-------lidar2zed start---------"<<endl;
 
 	Regi regi(n);
 
@@ -171,5 +174,6 @@ int main (int argc, char** argv)
 
 	return (0);
 }
+
 
 
